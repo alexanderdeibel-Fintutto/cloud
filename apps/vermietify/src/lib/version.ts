afd1283357ca44628bbba68d363c5bf7f4e318ae
alexander deibel <alexander.deibel@googlemail.com>
2026-04-24 02:19:04 +0200

    chore(vermietify): force Vercel redeploy - activate new ignoreCommand

---
+++ apps/vermietify/src/lib/version.ts
@@ -0,0 +1,4 @@
+// Auto-generated version file - forces Vercel redeploy
+// Last deploy trigger: 2026-04-24T00:19:04Z
+export const APP_VERSION = "1.0.0";
+export const BUILD_TIME = "2026-04-24T00:19:04.075283Z";
